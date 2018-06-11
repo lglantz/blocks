ef311b1eedbf570f28acaa9169a1e2178b7df56b
emma-floored <emma@floored.com>
2018-06-11 12:51:14 -0400

    add react index, update README to include subrepo instructions

--- README.md
+++ README.md
@@ -8,6 +8,21 @@ CBRE Building Blocks design system repository
 
 If you just want to use Blocks, you can copy the blocks.css file along with the fonts/ and svgs/ folders.
 
+Blocks can be included in your project as a subrepo by running
+```
+git clone https://github.com/floored/blocks /path/to/blocks
+```
+in the base folder of your project. So, for example, for DataVis, we included Blocks by running:
+```
+git clone https://github.com/floored/blocks /lib/blocks
+```
+
+If you want to use the included React components, they can be included by pointing to the react-index, ex:
+```
+const BlocksInput = require('../../../lib/blocks/react-index').Input;
+const Checkbox = BlocksInput.Checkbox;
+```
+
 ### Developing Blocks
 
 If you want to view the Blocks docs or develop Blocks, you'll want to download [Jekyll](https://jekyllrb.com/).
---
+++ react-index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  Accordion: require('./react/accordion/AccordionSection.jsx'),
+  Alerts: {
+    ErrorAlert: require('./react/alerts/ErrorAlert.jsx'),
+    InformationalAlert: require('./react/alerts/InformationalAlert.jsx'),
+    SuccessAlert: require('./react/alerts/SuccessAlert.jsx'),
+    WarningAlert: require('./react/alerts/WarningAlert.jsx')
+  },
+  Buttons: {
+    ButtonPrimary: require('./react/buttons/ButtonPrimary.jsx'),
+    ButtonSecondary: require('./react/buttons/ButtonSecondary.jsx'),
+    ButtonSmallPrimary: require('./react/buttons/ButtonSmallPrimary.jsx'),
+    ButtonSmallSecondary: require('./react/buttons/ButtonSmallSecondary.jsx')
+  },
+  Dropdowns: {
+    ComboBox: require('./react/dropdowns/ComboBox.jsx'),
+    Dropdown: require('./react/dropdowns/Dropdown.jsx')
+  },
+  Input: {
+    Checkbox: require('./react/input/Checkbox.jsx'),
+    RadioButton: require('./react/input/RadioButton.jsx'),
+    TextField: require('./react/input/TextField.jsx'),
+    Toggle: require('./react/input/Toggle.jsx')
+  },
+  Modals: {
+    ActionModal: require('./react/modals/ActionModal.jsx'),
+    BaseModal: require('./react/modals/BaseModal.jsx')
+  },
+  Progress: {
+    Breadcrumbs: require('./react/progress/Breadcrumbs.jsx'),
+    ProgressDots: require('./react/progress/ProgressDots.jsx')
+  },
+  SortableList: require('./react/lists/SortableList.jsx'),
+  Table: require('./react/table/Table.jsx'),
+  Tabs: {
+    HorizontalTabs: require('./react/tabs/HorizontalTabs.jsx'),
+    VerticalTabs: require('./react/tabs/VerticalTabs.jsx')
+  },
+  wrappers: {
+    closeOnClick: require('./react/wrappers/closeOnClick.jsx')
+  }
+};
