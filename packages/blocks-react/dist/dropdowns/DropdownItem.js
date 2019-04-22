06927f0f7fdd411ba358eca21956869048784aa3
Heather Wigmore <wigmore.heather@gmail.com>
2019-04-22 18:48:00 -0400

    remove preventDefault on DropdownItem, so that link dropdown items still navigate (#135)

--- packages/blocks-react/dist/dropdowns/DropdownItem.js
+++ packages/blocks-react/dist/dropdowns/DropdownItem.js
@@ -30,7 +30,6 @@ var DropdownItem = function (_React$Component) {
   _createClass(DropdownItem, [{
     key: 'onClick',
     value: function onClick(e) {
-      e.preventDefault();
       e.stopPropagation();
       this.props.onSelect(e, this.props.option);
     }
