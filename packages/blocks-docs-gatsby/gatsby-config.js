7118721ec18797a9c7a07b6fea10a6dc233c0127
emma-floored <emma@floored.com>
2019-05-20 09:27:09 -0500

    fix unsupported image format error message

--- packages/blocks-docs-gatsby/gatsby-config.js
+++ packages/blocks-docs-gatsby/gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#009f6f`,
+        theme_color: `#009f6f`,
         display: `minimal-ui`,
         icon: `${__dirname}/favicon.png`, // This path is relative to the root of the site.
       },
