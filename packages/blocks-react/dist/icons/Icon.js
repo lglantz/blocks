7a0bfee767365e2ec6d7aede841bb573ab2f33c3
JinsooLim <jinsoo@Jinsoo-MacBook-Pro.local>
2019-06-18 11:20:42 -0400

    use rest operator in Icon

--- packages/blocks-react/dist/icons/Icon.js
+++ packages/blocks-react/dist/icons/Icon.js
@@ -1,21 +1,25 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 var React = require('react');
 var PropTypes = require('prop-types');
 
 var Icon = function Icon(props) {
+  var children = props.children,
+      other = _objectWithoutProperties(props, ['children']);
+
   return React.createElement(
     'svg',
-    {
+    _extends({
       xmlns: 'http://www.w3.org/2000/svg',
       width: '20px',
       height: '20px',
-      viewBox: '0 0 20 20',
-      fill: props.fill,
-      className: props.className,
-      style: props.style
-    },
-    props.children
+      viewBox: '0 0 20 20'
+    }, other),
+    children
   );
 };
 
--- packages/blocks-react/icons/Icon.jsx
+++ packages/blocks-react/icons/Icon.jsx
@@ -3,17 +3,16 @@ const PropTypes = require('prop-types');
 
 
 const Icon = (props) => {
+  const { children, ...other } = props;
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="20px"
       height="20px"
       viewBox="0 0 20 20"
-      fill={props.fill}
-      className={props.className}
-      style={props.style}
+      {...other}
     >
-      {props.children}
+      {children}
     </svg>
   );
 }
